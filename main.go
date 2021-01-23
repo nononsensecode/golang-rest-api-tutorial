9b92c9993cf61a28e0f67053d28716a6751f4658
kaushik asokan <kaushikam@gmail.com>
2021-01-23 21:36:23 +0530

    Update method completed

--- main.go
+++ main.go
@@ -52,6 +52,22 @@ func deleteArticleByID(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hit endpoint updateArticle")
+	requestBody, _ := ioutil.ReadAll(r.Body)
+	var updatedArticle model.Article
+	json.Unmarshal(requestBody, &updatedArticle)
+	for index, article := range model.Articles {
+		if article.ID == updatedArticle.ID {
+			originalArticle := &model.Articles[index]
+			originalArticle.Title = updatedArticle.Title
+			originalArticle.Content = updatedArticle.Content
+			originalArticle.Desc = updatedArticle.Desc
+			json.NewEncoder(w).Encode(originalArticle)
+		}
+	}
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
@@ -59,6 +75,10 @@ func handleRequests() {
 	myRouter.HandleFunc("/article", createArticle).Methods("POST")
 	myRouter.HandleFunc("/article/{id}", returnArticleByID).Methods("GET")
 	myRouter.HandleFunc("/article/{id}", deleteArticleByID).Methods("DELETE")
+	myRouter.HandleFunc("/article", updateArticle).Methods("PUT")
+	//In the below code, as you can see method is not specified. So if we put this
+	//code above the delete method, delete will also give you get method result 
+	//instead of delete method
 	//myRouter.HandleFunc("/article/{id}", returnArticleByID)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
